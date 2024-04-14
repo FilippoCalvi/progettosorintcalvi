54a9f1ef32971bc18dc64040f1972915abc55f0a
NotArcade <calvifilippo2212@gmail.com>
2024-04-14 11:45:55 +0200

    c

--- src/common/Footer/Footer.jsx
+++ src/common/Footer/Footer.jsx
@@ -6,8 +6,13 @@ const Footer = () => {
     <footer>
       <div className='wrapper'>
         <div className="footer-column">
-          <img src="logo.png" alt="Logo" />
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <img src="https://www.mcdonalds.it/sites/default/files/styles/compressed/public/quality_stamp/bollo-qualita%CC%80-carne-pollo-italiana.png.webp?itok=4__4479Ong" alt="Logo" />
+          <img src="https://www.mcdonalds.it/sites/default/files/styles/compressed/public/quality_stamp/bollo-bacon.png.webp?itok=w31SoZcU" alt="Logo" />
+          <img src='https://www.mcdonalds.it/sites/default/files/2021-10/bollo-qualita-carne-bovina-italiana.png'alt = "Logo"/>
+          <img src='https://www.mcdonalds.it/sites/default/files/2021-10/bollo-latte.png'alt = "Logo"/>
+          <img src='https://www.mcdonalds.it/sites/default/files/2021-10/bollo-uova.png'alt = "Logo"/>
+          <img src='https://www.mcdonalds.it/sites/default/files/2021-10/bollo-albicocche.png'alt = "Logo"/>
+          <p>Copyright © 2024 McCalvi's. All rights reserved</p>
         </div>
         <div className="footer-column"></div>
         <div className="footer-column">
